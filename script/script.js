5c61ba7c08554a44af19359bdd506ed997fcc774
Eduardo Nagashima <eduyudji@gmail.com>
2022-02-04 12:30:19 -0300

    projeto semi completo, consegui inventar uma lógica de distribuição de cartas através de um array, não é possível clicar várias vezes na mesma carta, jogo acaba

--- script/script.js
+++ script/script.js
@@ -1,7 +1,23 @@
 let cardsNumber;
 let contPlays = 0;
-let cards = []
-let outraArray = []
+let imgArray = []
+let qntMatch = 0;
+imgArray.push("images/explodyparrot.gif");
+imgArray.push("images/explodyparrot.gif");
+imgArray.push('images/bobrossparrot.gif');
+imgArray.push('images/bobrossparrot.gif');
+imgArray.push('images/fiestaparrot.gif');
+imgArray.push('images/fiestaparrot.gif');
+imgArray.push('images/metalparrot.gif');
+imgArray.push('images/metalparrot.gif');
+imgArray.push('images/revertitparrot.gif');
+imgArray.push('images/revertitparrot.gif');
+imgArray.push('images/tripletsparrot.gif');
+imgArray.push('images/tripletsparrot.gif');
+imgArray.push('images/unicornparrot.gif');
+imgArray.push('images/unicornparrot.gif');
+
+// 5 - se todas derem "matchs", acabar o jogo!
 
 function playGame() {
     cardsNumber = 4;
@@ -10,47 +26,65 @@ function playGame() {
     // while (cardsNumber % 2 == 1 || cardsNumber > 14 || cardsNumber < 4) {
     //     cardsNumber = parseInt(prompt('Com quantas cartas quer jogar? (4 à 14)'));
     // }
+
+    imgArray = imgArray.slice(0, cardsNumber);
+    imgArray.sort(comparador);
     dealingCards();
 }
 
 function dealingCards() {
     cartas = document.querySelector(".cards");
-
-    for (let cont = 0; cont < cardsNumber; cont++) {
-        cards.push(cont);
-    }
-
-    cards.sort(comparador);
     for (let cont = 0; cont < cardsNumber; cont++) {
         cartas.innerHTML += `
         <div class="card" onclick=turn(this)>
             <div class="face front-face">
-                <img src="image/parrot.png" alt="carta-parrot">
+                <img src="images/parrot.png" alt="carta-parrot">
             </div>
             <div class="back-face face">
-                Verso ${cards[cont]}
+                <img src="${imgArray[cont]}">
             </div>
         </div>`
     }
-
 }
 
 function turn(el) {
-    turnCard(el);
-    el.classList.add('flipped');
-    if (el.classList.contains('flipped')) {
-        // achar uma condição para comparar duas cartas
-
-    } else {
-        setTimeout(function esperar() {
-            turn(el);
-            el.classList.remove('flipped')
-        }, 2000);
+    let flippeds = document.querySelectorAll(".flipped");
+    // conferir pra ver se tem 2 levantadas, se não não deixar levantar mais de 2
+    if (flippeds.length < 2) {
+        turnCard(el);
+        el.classList.add('flipped');
+        el.style.pointerEvents = 'none';
+        let comparationArray = document.querySelectorAll('.flipped');
+        console.log(comparationArray);
+        if (comparationArray.length == 2) {
+            if (comparationArray[0].innerHTML == comparationArray[1].innerHTML) {
+                match();
+            } else {
+                setTimeout(turnBack, 500);
+            }
+        }
+        contPlays++;
+        refreshCount();
     }
-
-    contPlays++;
-    refreshCount();
 }
+
+
+function match() {
+    let flippeds = document.querySelectorAll(".flipped");
+
+    for (let i = 0; i < flippeds.length; i++) {
+        flippeds[i].classList.remove("flipped");
+        flippeds[i].classList.add("matched");
+        flippeds[i].onclick = '';
+        qntMatch++;
+    }
+    console.log(qntMatch);
+    if (qntMatch === (cardsNumber)) {
+        alert('ACABOU O JOGO!');
+    }
+}
+
+
 
 function turnCard(el) {
     const cardFront = el.querySelector('.front-face');
@@ -59,7 +93,14 @@ function turnCard(el) {
     cardFront.classList.toggle('turn-front');
 }
 
-
+function turnBack() {
+    let flipped = document.querySelectorAll(".flipped");
+    for (let i = 0; i < flipped.length; i++) {
+        flipped[i].classList.remove("flipped");
+        flipped[i].style.pointerEvents = 'auto';
+        turnCard(flipped[i]);
+    }
+}
 
 function comparador() {
     return Math.random() - 0.5;
